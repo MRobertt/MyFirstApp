88b25fa7d268c310d2e5c642ab7c22fa481b2eb1
eXpert <eXpert@DESKTOP-VFUUIRB>
2020-10-08 19:41:00 +0300

    HELLO !

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+            Console.WriteLine("Hello World !!");
+            |
         }
     }
 }
